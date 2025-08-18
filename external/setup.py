aed374873047e5d281b3d80bd9839bcaf37bf51b
NotFilthyShafay <mshafay.bee21seecs@seecs.edu.pk>
2025-08-18 17:48:04 +0500

    Fix Cython and MSVC compile errors for nms extension

--- external/setup.py
+++ external/setup.py
@@ -1,18 +1,23 @@
-import numpy
-from distutils.core import setup
-from distutils.extension import Extension
+import numpy as np
+from setuptools import setup, Extension
 from Cython.Build import cythonize
+import sys
+
+if sys.platform == 'win32':
+    extra_compile_args = []
+else:
+    extra_compile_args = ['-Wno-cpp', '-Wno-unused-function']
 
 extensions = [
     Extension(
         "nms", 
         ["nms.pyx"],
-        extra_compile_args=["-Wno-cpp", "-Wno-unused-function"]
+        extra_compile_args=extra_compile_args
     )
 ]
 
 setup(
     name="coco",
     ext_modules=cythonize(extensions),
-    include_dirs=[numpy.get_include()]
+    include_dirs=[np.get_include()]
 )
