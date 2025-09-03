e4257e4d1376e6386860f6c09915cff62550217f
cleanshafay <mshafay.bee21seecs@seecs.edu.pk>
2025-09-03 12:17:31 +0500

    django url import changed

--- server_match/urls.py
+++ server_match/urls.py
@@ -13,8 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import re_path
 from . import view
+
 urlpatterns = [
-    url(r'^$', view.get_group),
+    re_path(r'^$', view.get_group),
 ]
